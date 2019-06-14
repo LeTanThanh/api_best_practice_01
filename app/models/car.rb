84f5cd4091eb8823a1afb88afdd1587dd9b4e061
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-06-14 11:13:48 +0700

    add_car_validates

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,6 @@
 class Car < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :color, presence: true
+  validates :code, presence: true, uniqueness: true
+  validates :description, presence: true
 end
