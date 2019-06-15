360c67a94a676e45fb49984a2ff354dcb9b43b2b
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-06-15 19:06:44 +0700

    add_api/v1/cars#show

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CarsController < Api::V1::ApplicationController
+  before_action :load_car, only: %i(show update destroy)
+
   def create
   end
 
@@ -8,11 +10,18 @@ class Api::V1::CarsController < Api::V1::ApplicationController
   end
 
   def show
+    render json: @car
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private 
+
+  def load_car
+    @car = Car.find params[:id]
   end
 end
