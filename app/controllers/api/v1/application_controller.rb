0711d85c30708142e50723dc5e81c2769ea8171a
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-06-15 20:07:47 +0700

    add_api/v1/cars_create_index_update_destroy

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,3 +1,22 @@
 class Api::V1::ApplicationController < ApplicationController
   protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  
+  private
+
+  def record_not_found error
+    render json: {
+      success: false,
+      message: error.message
+    }
+  end
+
+  def record_invalid error
+    render json: {
+      success: false,
+      message: error.message
+    }
+  end
 end
--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -2,26 +2,69 @@ class Api::V1::CarsController < Api::V1::ApplicationController
   before_action :load_car, only: %i(show update destroy)
 
   def create
+    @car = Car.create! car_params
+
+    render json: {
+      success: true,
+      message: "Create car success",
+      data: {
+        car: @car
+      }
+    }
   end
 
   def index
     @cars = Car.all
-    render json: @cars
+
+    render json: {
+      success: true,
+      message: "Get list cars success",
+      data: {
+        cars: @cars
+      }
+    }
   end
 
   def show
     render json: @car
+
+    render json: {
+      success: true,
+      message: "Get car success",
+      data: {
+        car: @car
+      }
+    }
   end
 
   def update
+    @car.update! car_params
+
+    render json: {
+      success: true,
+      message: "Update car success",
+      data: {
+        car: @car
+      }
+    }
   end
 
   def destroy
+    @car.destroy
+
+    render json: {
+      success: true,
+      message: "Delete car success"
+    }
   end
 
   private 
 
   def load_car
     @car = Car.find params[:id]
+  end
+
+  def car_params
+    params.require(:car).permit :name, :color, :code, :description
   end
 end
