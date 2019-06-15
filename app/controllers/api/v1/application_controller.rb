71c5ec222fb3f850a7766064da3edead35c0b4cb
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-06-16 00:08:53 +0700

    add_controller/concern/rescue_from_error.rb

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,19 +1,5 @@
 class Api::V1::ApplicationController < ApplicationController
+  include RescueFromError
+
   protect_from_forgery with: :null_session
-
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
-
-  private
-
-  def record_not_found error
-    i18n_path = params[:controller].split("/").join(".")
-    message = I18n.t "#{i18n_path}.not_found"
-
-    render json: Errors::RecordNotFoundError.new(error, message).to_hash, status: :not_found
-  end
-
-  def record_invalid error
-    render json: Errors::RecordInvalidError.new(error).to_hash, status: :unprocessable_entity
-  end
 end
---
+++ app/controllers/concerns/rescue_from_error.rb
@@ -0,0 +1,19 @@
+module RescueFromError extend ActiveSupport::Concern
+  included do
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  end
+
+  private
+
+  def record_not_found error
+    i18n_path = params[:controller].split("/").join(".")
+    message = I18n.t "#{i18n_path}.not_found"
+
+    render json: Errors::RecordNotFoundError.new(error, message).to_hash, status: :not_found
+  end
+
+  def record_invalid error
+    render json: Errors::RecordInvalidError.new(error).to_hash, status: :unprocessable_entity
+  end
+end
